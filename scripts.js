d5b3ff85f7cb19841e97588fd153a73cb5b974ae
Samuellevine1 <SL@samuellevine.com>
2020-10-08 11:48:14 +0000

    finishing js to unflip cards and reset and shuffle

--- scripts.js
+++ scripts.js
@@ -21,3 +21,40 @@ function flipCard() {
 
   checkForMatch();
 }
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.ceil(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
